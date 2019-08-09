fafb1cde8bad950de9173071f82f619c05a30558
mlkone <martin.kuenzel@hs-bochum.de>
2019-08-09 13:16:44 +0200

    added todo component and model

---
+++ angular-crashcourse-todolist/src/app/components/todo/todo.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  todoItems: Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todoItems = [
+      {
+        id: 0,
+        title: 'todo 1',
+        content: 'this is the content'
+      },
+      {
+        id: 1,
+        title: 'todo 2',
+        content: 'this is the content'
+      },
+      {
+        id: 2,
+        title: 'todo 3',
+        content: 'this is the content'
+      },
+    ]
+  }
+
+}
